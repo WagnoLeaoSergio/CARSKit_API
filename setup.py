5f5be3404f652fd13770438246d9ffefbb5ca7d0
Wagno <wagnoleao@gmail.com>
2020-09-16 16:42:26 -0300

    README, MANIFEST and CHANGES files created

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
+    # Application name
     name="carskit_api",
+    # Version number
     version="0.0.1",
     description="An API for the CARSKit engine",
+    # Application author details
     author="WagnoLeaoSergio",
     author_email="wagnoleao@gmail.com",
+    # Dependencies
     install_requires=["cliff"],
+    # Packages
     packages=find_packages(),
+
+    # Details
+    #url  = ""
     include_package_data=True,
     entry_points={
         "console_scripts": ["capi = carskit_api.cli.main:main"],
