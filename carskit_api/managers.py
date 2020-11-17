670a53214f11af74accc0ff1f6cdc0a0274b8136
Wagno <wagnoleao@gmail.com>
2020-11-17 15:23:05 -0300

    test_data_manager atualizado

--- carskit_api/managers.py
+++ carskit_api/managers.py
@@ -34,8 +34,7 @@ class Model_Statistics_Manager(object):
     def __init__(self, output_folder: str, statistics_filename="sample@statistics"):
         self.statistics_filename = statistics_filename
         self.output_folder = output_folder
-        self.current_path = pl.Path(
-            os.path.dirname(os.path.abspath(__file__)))
+        self.current_path = pl.Path(os.path.dirname(os.path.abspath(__file__)))
 
     def generate_statistic_data(self, statistic_file) -> dict:
         """
@@ -68,54 +67,27 @@ class Model_Statistics_Manager(object):
 
         fields_founded = False
         stats_data = {
-            " * User amount": {
-                "field_name": "user_amount",
-                "value": 0
-            },
-            " * Item amount": {
-                "field_name": "item_mount",
-                "value": 0
-            },
-            " * Rate amount": {
-                "field_name": "rate_amount",
-                "value": 0
-            },
-            " * Context dimensions": {
-                "field_name": "context_dimensions",
-                "value": 0
-            },
-            " * Context conditions": {
-                "field_name": "context_conditions",
-                "value": 0
-            },
-            " * Context situations": {
-                "field_name": "context_situations",
-                "value": 0
-            },
-            " * Data density": {
-                "field_name": "data_density",
-                "value": 0
-            },
-            " * Scale distribution": {
-                "field_name": "scale_distributions",
-                "value": ""
-            },
+            " * User amount": {"field_name": "user_amount", "value": 0},
+            " * Item amount": {"field_name": "item_mount", "value": 0},
+            " * Rate amount": {"field_name": "rate_amount", "value": 0},
+            " * Context dimensions": {"field_name": "context_dimensions", "value": 0},
+            " * Context conditions": {"field_name": "context_conditions", "value": 0},
+            " * Context situations": {"field_name": "context_situations", "value": 0},
+            " * Data density": {"field_name": "data_density", "value": 0},
+            " * Scale distribution": {"field_name": "scale_distributions", "value": ""},
             " * Average value of all ratings": {
                 "field_name": "average_ratings",
-                "value": 0
+                "value": 0,
             },
             " * Standard deviation of all ratings": {
                 "field_name": "standard_deviation_ratings",
-                "value": 0
+                "value": 0,
             },
-            " * Mode of all rating values": {
-                "field_name": "mode_ratings",
-                "value": 0
-            },
+            " * Mode of all rating values": {"field_name": "mode_ratings", "value": 0},
             " * Median of all rating values": {
                 "field_name": "median_ratings",
-                "value": 0
-            }
+                "value": 0,
+            },
         }
 
         try:
@@ -126,8 +98,7 @@ class Model_Statistics_Manager(object):
                     field = re.split(":", field, 1)
 
                     if field[0] in stats_data:
-                        stats_data[field[0]]["value"] = re.sub(
-                            " ", "", field[1])
+                        stats_data[field[0]]["value"] = re.sub(" ", "", field[1])
                         fields_founded = True
         except KeyError as e:
             return e
@@ -137,7 +108,7 @@ class Model_Statistics_Manager(object):
 
         labeled_data = {}
         for field in stats_data.items():
-            labeled_data[field[1]['field_name']] = field[1]['value']
+            labeled_data[field[1]["field_name"]] = field[1]["value"]
 
         return labeled_data
 
@@ -154,27 +125,40 @@ class Model_Statistics_Manager(object):
             A message about the success of the operation.
         """
 
-        if "average_ratings" in stats_data and "median_ratings" in stats_data \
-                and "mode_ratings" in stats_data and "standard_deviation_ratings" in stats_data \
-                and "scale_distributions" in stats_data:
+        if (
+            "average_ratings" in stats_data
+            and "median_ratings" in stats_data
+            and "mode_ratings" in stats_data
+            and "standard_deviation_ratings" in stats_data
+            and "scale_distributions" in stats_data
+        ):
 
             stats_data["average_ratings"] = re.sub(
-                ",", ".", stats_data["average_ratings"])
+                ",", ".", stats_data["average_ratings"]
+            )
             stats_data["median_ratings"] = re.sub(
-                ",", ".", stats_data["median_ratings"])
-            stats_data["mode_ratings"] = re.sub(
-                ",", ".", stats_data["mode_ratings"])
+                ",", ".", stats_data["median_ratings"]
+            )
+            stats_data["mode_ratings"] = re.sub(",", ".", stats_data["mode_ratings"])
             stats_data["standard_deviation_ratings"] = re.sub(
-                ",", ".", stats_data["standard_deviation_ratings"])
+                ",", ".", stats_data["standard_deviation_ratings"]
+            )
 
             stats_data["scale_distributions"] = re.split(
-                ",",  stats_data["scale_distributions"])
+                ",", stats_data["scale_distributions"]
+            )
 
             stats_data["scale_distributions"][0] = re.sub(
-                r"\[|\]", "", stats_data["scale_distributions"][0])
-            stats_data["scale_distributions"][len(stats_data["scale_distributions"]) - 1] = re.sub(
-                r"\[|\]", "", stats_data["scale_distributions"][len(
-                    stats_data["scale_distributions"]) - 1]
+                r"\[|\]", "", stats_data["scale_distributions"][0]
+            )
+            stats_data["scale_distributions"][
+                len(stats_data["scale_distributions"]) - 1
+            ] = re.sub(
+                r"\[|\]",
+                "",
+                stats_data["scale_distributions"][
+                    len(stats_data["scale_distributions"]) - 1
+                ],
             )
 
             return "statistics data successfully formatted!"
@@ -214,7 +198,7 @@ class Model_Statistics_Manager(object):
             json.dump(stats_data, statistics_file)
             statistics_file.close()
 
-            return f"The file {file_name + '.json'} was successfully created!"
+            return f"The file {file_name} was successfully created!"
 
         return f"ERROR! The file {stats_file_path} is not readable."
 
@@ -238,20 +222,19 @@ class Recommendations_Manager(object):
 
     def __init__(self, output_folder: str, result_filename="sample_recommendations"):
 
-        self.current_path = pl.Path(
-            os.path.dirname(os.path.abspath(__file__)))
+        self.current_path = pl.Path(os.path.dirname(os.path.abspath(__file__)))
         self.output_folder = output_folder
         self.result_filename = result_filename
 
     def get_line_header(self, line: str):
         """
-        Returns the user's id and 
+        Returns the user's id and
         a list of his contexts from the specified line.
         """
 
         contexts = []
 
-        s_line = line.split(sep=',')
+        s_line = line.split(sep=",")
         user = s_line[0]
         context_str = s_line[1]
         context_list = re.findall(".*?;|.*?: ", context_str)
@@ -281,7 +264,7 @@ class Recommendations_Manager(object):
 
             s_rec = re.sub("[(]|[)]", "", rec)
             s_rec = re.sub(" ", "", s_rec)
-            s_rec = s_rec.split(sep=',')
+            s_rec = s_rec.split(sep=",")
             item = s_rec[0]
             rating = s_rec[1]
 
@@ -291,7 +274,7 @@ class Recommendations_Manager(object):
 
     def create_context_combinations(self, contexts: list) -> str:
         """
-        Returns the context combination string from a 
+        Returns the context combination string from a
         specified list of contexts.
         """
 
@@ -312,9 +295,7 @@ class Recommendations_Manager(object):
                 The recommendations file loaded.
         """
 
-        results_data = {
-            "users": {}
-        }
+        results_data = {"users": {}}
 
         header = recs_file.readline()
         for line in recs_file:
@@ -342,7 +323,7 @@ class Recommendations_Manager(object):
             recs_data: dict
                 The recommendation that was extrated from the engine's file.
 
-            recs_file_path: 
+            recs_file_path:
                 The path where the recommendations will be saved.
         """
 
@@ -363,6 +344,6 @@ class Recommendations_Manager(object):
             json.dump(recs_data, recommendations_file)
             recommendations_file.close()
 
-            return f"The file {file_name + '.json'} was successfully created!"
+            return f"The file {file_name} was successfully created!"
 
         return f"ERROR! The file {recs_file_path} is not readable."
--- carskit_api/tests/test_data_manager.py
+++ carskit_api/tests/test_data_manager.py
@@ -1,23 +1,34 @@
+import os
 import pytest
-from data_manager import Model_Statistics_Manager, Recommendations_Manager
+from ..managers import Model_Statistics_Manager, Recommendations_Manager
+from ..controllers.data_processing import get_app_path
+
+
+def tests_path():
+    return os.path.join(get_app_path(), "tests/")
+
 
 @pytest.fixture
 def sample_Model_Statistics_Manager():
-    return Model_Statistics_Manager()
+    return Model_Statistics_Manager(output_folder="results")
+
 
 @pytest.fixture
 def sample_Recommendations_Manager():
-    return Recommendations_Manager()
+    return Recommendations_Manager(output_folder="results")
+
 
 @pytest.fixture
 def sample_stats_file():
-    stats_file = open("./source/datasets/results/sample@statistics.txt")
+    stats_file = open(tests_path() + "sample_statistics.txt")
     return stats_file
+
 
 @pytest.fixture
 def sample_recommendations_file():
-    rec_file = open("./source/datasets/results/sample_recommendations.txt")
+    rec_file = open(tests_path() + "/sample_recommendations.txt")
     return rec_file
+
 
 """
 ##############################
@@ -25,69 +36,123 @@ Model_Statistics_Manager Tests
 ##############################
 """
 
+
+def formatted_valid_stats_data():
+    return {
+        "user_amount": "97",
+        "item_mount": "79",
+        "rate_amount": "5035",
+        "context_dimensions": "3(time,location,companion)",
+        "context_conditions": "10(time:3,location:3,companion:4)",
+        "context_situations": "13",
+        "data_density": "1.8230%",
+        "scale_distributions": [
+            "1.0x829",
+            "2.0x625",
+            "4.0x1209",
+            "5.0x1367",
+            "3.0x1005",
+        ],
+        "average_ratings": "3.328892",
+        "standard_deviation_ratings": "1.414777",
+        "mode_ratings": "5.000000",
+        "median_ratings": "4.000000",
+    }
+
+
 def non_formatted_valid_stats_data():
     return {
-        'average_ratings': '3,328892',
-        'context_conditions': '10(time:3,location:3,companion:4)',
-        'context_dimensions': '3(time,location,companion)',
-        'context_situations': '13',
-        'data_density': '1,8230%',
-        'item_mount': '79',
-        'median_ratings': '4,000000',
-        'mode_ratings': '5,000000',
-        'rate_amount': '5035',
-        'scale_distributions': '[1.0x829,2.0x625,4.0x1209,5.0x1367,3.0x1005]',
-        'standard_deviation_ratings': '1,414777',
-        'user_amount': '97'
+        "user_amount": "97",
+        "item_mount": "79",
+        "rate_amount": "5035",
+        "context_dimensions": "3(time,location,companion)",
+        "context_conditions": "10(time:3,location:3,companion:4)",
+        "context_situations": "13",
+        "data_density": "1.8230%",
+        "scale_distributions": "[1.0x829,2.0x625,4.0x1209,5.0x1367,3.0x1005]",
+        "average_ratings": "3.328892",
+        "standard_deviation_ratings": "1.414777",
+        "mode_ratings": "5.000000",
+        "median_ratings": "4.000000",
     }
+
 
 def non_formated_invalid_stats_data():
     return {
-        'average_ratings': '3,328892',
-        'context_conditions': '10(time:3,location:3,companion:4)',
-        'context_dimensions': '3(time,location,companion)',
-        'context_situations': '13',
-        'data_density': '1,8230%',
-        'item_mount': '79',
-        'mode_ratings': '5,000000',
-        'rate_amount': '5035',
-        'scale_distributions': '[1.0x829,2.0x625,4.0x1209,5.0x1367,3.0x1005]',
-        'user_amount': '97'
+        "average_ratings": "3,328892",
+        "context_conditions": "10(time:3,location:3,companion:4)",
+        "context_dimensions": "3(time,location,companion)",
+        "context_situations": "13",
+        "data_density": "1,8230%",
+        "item_mount": "79",
+        "mode_ratings": "5,000000",
+        "rate_amount": "5035",
+        "scale_distributions": "[1.0x829,2.0x625,4.0x1209,5.0x1367,3.0x1005]",
+        "user_amount": "97",
     }
+
 
 ########## format_stats(self, stats_data)
 
 
-@pytest.mark.parametrize("stats_data, expected_result", [
-    (
-        non_formatted_valid_stats_data(),
-        "statistics data successfully formatted!"
-    ),
-    (
-        non_formated_invalid_stats_data(),
-        "ERROR! statistics data dict not supported"
-    )
-])
+@pytest.mark.parametrize(
+    "stats_data, expected_result",
+    [
+        (non_formatted_valid_stats_data(), "statistics data successfully formatted!"),
+        (
+            non_formated_invalid_stats_data(),
+            "ERROR! statistics data dict not supported",
+        ),
+    ],
+)
 def test_format_stats(sample_Model_Statistics_Manager, stats_data, expected_result):
-    assert sample_Model_Statistics_Manager.format_stats(stats_data) == expected_result
+    formating_status = sample_Model_Statistics_Manager.format_stats(stats_data)
+    assert formating_status == expected_result
 
 
 ########## generate_statistic_data(self, statistic_file):
 
 
 def test_generate_statistic_data(sample_Model_Statistics_Manager, sample_stats_file):
-    assert sample_Model_Statistics_Manager.generate_statistic_data(sample_stats_file) == non_formatted_valid_stats_data()
+    generated_data = sample_Model_Statistics_Manager.generate_statistic_data(
+        sample_stats_file
+    )
+    assert isinstance(generated_data, dict) and all(
+        k in generated_data
+        for k in (
+            "user_amount",
+            "item_mount",
+            "rate_amount",
+            "context_dimensions",
+            "context_conditions",
+            "context_situations",
+            "data_density",
+            "scale_distributions",
+            "average_ratings",
+            "standard_deviation_ratings",
+            "mode_ratings",
+            "median_ratings",
+        )
+    )
 
 
 ########## save_statistics(self):
-@pytest.mark.parametrize("file_path, expected_result",[
-    ("./source/datasets/results/sample@statistics.txt",
-    "The file execution_statistics.json was successfully created!"),
-    ("./source/datasets/results/avbnbqd.txt",
-    "ERROR! The file ./source/datasets/results/avbnbqd.txt could not be opened.")
-])
+@pytest.mark.parametrize(
+    "file_path, expected_result",
+    [
+        (
+            tests_path() + "sample@statistics.json",
+            "The file sample@statistics.json was successfully created!",
+        ),
+    ],
+)
 def test_save_statistics(sample_Model_Statistics_Manager, file_path, expected_result):
-    assert sample_Model_Statistics_Manager.save_statistics(file_path) == expected_result
+    assert (
+        sample_Model_Statistics_Manager.save_statistics(
+            non_formatted_valid_stats_data(), file_path
+        )
+        == expected_result
+    )
 
 
 """
@@ -99,16 +164,29 @@ Recommendations_Manager Tests
 
 ########## save_recommendations(self):
 
-@pytest.mark.parametrize("results_file, expected_result", [
-    (
-        "./source/datasets/results/sample_recommendations.txt",
-    "The file sample_recommendations.json was successfully created!"
-    ),
-    (
-        "./source/datasets/results/edcveadw.txt",
-        "ERROR! The file ./source/datasets/results/edcveadw.txt could not be opened."
-    )
-])
+
+@pytest.mark.parametrize(
+    "results_filepath, expected_result",
+    [
+        (
+            tests_path() + "sample@recommendations.json",
+            "The file sample@recommendations.json was successfully created!",
+        ),
+    ],
+)
 @pytest.mark.data_writing
-def test_save_recommendations(sample_Recommendations_Manager, results_file, expected_result):
-    assert sample_Recommendations_Manager.save_recommendations(results_file) == expected_result
+def test_save_recommendations(
+    sample_Recommendations_Manager,
+    sample_recommendations_file,
+    results_filepath,
+    expected_result,
+):
+    sample_recs = sample_Recommendations_Manager.generate_recommendations_data(
+        sample_recommendations_file
+    )
+    assert (
+        sample_Recommendations_Manager.save_recommendations(
+            sample_recs, results_filepath
+        )
+        == expected_result
+    )
